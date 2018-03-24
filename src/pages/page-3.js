d515b41f575debbcfded0c50488acb6d71125501
clickingmouse <clickingmouse@users.noreply.github.com>
2018-03-24 15:33:16 +0800

    first commit

---
+++ src/pages/page-3.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "gatsby-Link";
+
+const ThirdPage =()=>(
+<div>
+<h1>Third Page</h1>
+<p>This is my first Gatsby site</p>
+<Link to="/page-2/">Back to Page 2</Link>
+<br/>
+<Link to="/">Go back to the homepage</Link>
+</div>
+);
+export default ThirdPage;
+
+
+
